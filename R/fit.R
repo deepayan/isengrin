0c902da146574760ee85268be16d122b95be8031
Deepayan Sarkar <deepayan.sarkar@gmail.com>
2026-02-07 18:33:05 +0530

    add sparse implementation, support for A

--- R/fit.R
+++ R/fit.R
@@ -48,7 +48,7 @@ constructL_1d_dense <- function(p, filter)
 
 constructL_1d_sparse <- function(p, filter)
 {
-    constructL_1d_dense(p, filter) |> Matrix::sparseMatrix()
+    constructL_1d_dense(p, filter) |> Matrix::Matrix()
 }
 
 
@@ -82,6 +82,7 @@ lspen_dense <- function(x, y, n = 1, L, L.method = "d1",
             sqrtW <- irls_wts(L %*% mu_hat)
             mu_hat <- solve(N + lambda * crossprod(sqrtW * L), n * y)
         }
+        sqrtW <- irls_wts(L %*% mu_hat) # for final estimate
         BtB <- tcrossprod((1/sqrt(n)) * t(sqrtW * L))
         eta1 <- eigen(BtB, symmetric = TRUE, only.values = TRUE)$values
         enp <- lambda2df(lambda, eta1)
@@ -89,14 +90,98 @@ lspen_dense <- function(x, y, n = 1, L, L.method = "d1",
     else 
         enp <- lambda2df(lambda, eta)
 
-    if (verbose) message("Equiv d.f. (trace(H)): ", enp)
+    if (verbose) message("Equiv d.f. (trace(H)) = ", format(enp),
+                         " with λ = ", format(lambda))
 
-    structure(list(fitted = mu_hat,
+    structure(list(x = x, y = y, n = n,
+                   fitted = mu_hat,
                    lambda = lambda,
                    enp = enp,
                    niter = niter),
               class = c("lpen"))
 }
 
+
+## general method, delegating sparse / dense to Matrix. TODO: calculate enp = tr(H)
+
+lspen <-
+    function(x, y, n = 1, L, A = NULL, L.method = "d1",
+             df = NULL, lambda = NULL,
+             niter = 0, verbose = TRUE)
+{
+    p <- length(y)
+    stopifnot("'n' must be scalar or have same length as y" = length(n) %in% c(1, p))
+    if (length(n) == 1) n <- rep(n, p)
+    N <- Matrix::Diagonal(x = n)
+    sqrt_n <- sqrt(n)
+    if (!is.null(A)) {
+        A <- Matrix::Matrix(A)
+        stopifnot("Mismatch in dim(A)" = all.equal(dim(A), c(p, length(x))))
+        if (is.null(lambda)) stop("lambda must be specified (for now) if A ≠ I")
+    }
+    stopifnot("'n' must be scalar or have same length as y" = length(n) %in% c(1, p))
+
+    if (missing(L)) {
+        stopifnot("Unsupported 'L.method'" = L.method %in% c("d1", "d2"))
+        filter <- switch(L.method, d1 = c(-1, 1), d2 = c(1, -2, 1))
+        L <- constructL_1d_sparse(length(x), filter)
+    }
+
+    if (missing(lambda)) {
+        likely_rank <- min(dim(L))
+        BtB <- tcrossprod((1/sqrt(n)) * t(L))
+        eta <- eigen(BtB, symmetric = TRUE, only.values = TRUE)$values
+
+        if (missing(df)) df <- sqrt(sum(n)) # default df = sqrt(nobs)
+        lambda <- df2lambda(df, eta = eta, rank = likely_rank)
+    }
+
+    mu_hat <-
+        if (is.null(A))
+            solve(N + lambda * crossprod(L), n * y) # or N %*% y
+        else 
+            solve(crossprod(sqrt_n * A) + lambda * crossprod(L), crossprod(A, n * y))
+
+    if (niter) {
+        for (i in seq_len(niter)) {
+            sqrtW <- irls_wts(L %*% mu_hat)
+            mu_hat <-
+                if (is.null(A))
+                    solve(N + lambda * crossprod(sqrtW * L), n * y)
+                else
+                    solve(crossprod(sqrt_n * A) + lambda * crossprod(sqrtW * L),
+                          crossprod(A, n * y))
+        }
+        sqrtW <- irls_wts(L %*% mu_hat) # for final estimate
+        if (is.null(A)) {
+            BtB <- tcrossprod((1/sqrt_n) * t(sqrtW * L))
+            eta1 <- eigen(BtB, symmetric = TRUE, only.values = TRUE)$values
+            enp <- lambda2df(lambda, eta1)
+        }
+        else { # this works even when A = I [TODO: compare]
+            C <- crossprod(sqrt_n * A)
+            enp <- sum(diag(solve(C + lambda * crossprod(sqrtW * L), C)))
+        }
+    }
+    else 
+        if (is.null(A)) {
+            enp <- lambda2df(lambda, eta)
+        } else
+        {
+            C <- crossprod(sqrt_n * A)
+            enp <- sum(diag(solve(C + lambda * crossprod(L), C)))
+        }
+
+    if (verbose) message("Equiv d.f. (trace(H)) = ", format(enp),
+                         " with λ = ", format(lambda))
+
+    structure(list(x = x, y = y, n = n,
+                   fitted = as.vector(mu_hat),
+                   lambda = lambda,
+                   enp = enp,
+                   niter = niter),
+              class = c("lpen"))
+
+}
 
 
